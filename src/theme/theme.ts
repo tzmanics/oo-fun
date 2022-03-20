935036f297522f4333c1c822176947bc7bdbb149
camiloriosf <camilo.rios.f@gmail.com>
2022-03-20 19:17:02 -0300

    Update card side variant for large viewports

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -77,6 +77,12 @@ const theme = createTheme({
               margin: 24,
               boxShadow:
                 "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
+              [baseTheme.breakpoints.down("xl")]: {
+                height: 320,
+              },
+              [baseTheme.breakpoints.down("lg")]: {
+                height: 240,
+              },
               [baseTheme.breakpoints.down("md")]: {
                 margin: 0,
                 flex: "auto",
